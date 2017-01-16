735b1ee399f83308015ea9be70809dd3f7aba17c
Hemant <hemantmanwani.iitr@gmail.com>
2017-01-16 15:14:05 +0530

    Angular file uploader modularized

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
-﻿var appRoot = angular.module('file-upload-demo', []);     //Define the main module
+
+
+console.log(angularFileUploader);
+var app = angular.module('app',['angular-file-uploader']);
+
+
+
+
+app.config(["fileuploadProvider", function(fileuploadProvider){
+  fileuploadProvider.configure({
+    url: "http://54.169.218.46:3005/signedUrl"
+  });
+}]);
+
+console.log("hello");
+
+app.controller('fileUploadController', ['$scope', function ($scope) {
+  $scope.files = [];
+  $scope.fileUploaded = function(data){
+    console.log(data);
+  }
+  $scope.fileNotUploaded = function(err){
+    console.log(err);
+  }
+}]);
